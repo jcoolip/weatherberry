367a34fa0605a991d76436cb7224a773f0859cd6
Justin <jcoolip@gmail.com>
2026-03-23 00:29:39 -0400

    tabs and spaces...

--- capture.py
+++ capture.py
@@ -1,21 +1,25 @@
-from picamera2 import Picamera2
 import time
 from datetime import datetime
 
+from picamera2 import Picamera2
+
+
 def capture():
-	try:
-	    now = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
-		cam = Picamera2()
+    now = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
+    try:
+        cam = Picamera2()
 
-		cam_config = cam.create_still_configuration(main={"size": (1920,1080)}, buffer_count=1)
-		cam.configure(cam_config)
+        cam_config = cam.create_still_configuration(
+            main={"size": (1920, 1080)}, buffer_count=1
+        )
+        cam.configure(cam_config)
 
-		cam.start()
+        cam.start()
 
-		time.sleep(2)
+        time.sleep(2)
 
-		cam.capture_file(f"./data/images/{now}.jpg")
-		cam.stop()
-		print(f"saved as ./data/images/{now}.jpg")
-	except Exception as e:
-		print(f"error: {e}")
+        cam.capture_file(f"./data/images/{now}.jpg")
+        cam.stop()
+        print(f"saved as ./data/images/{now}.jpg")
+    except Exception as e:
+        print(f"error: {e}")
