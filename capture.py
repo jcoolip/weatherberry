c0623115facb34adbd6541eda844cbe953b4e329
Justin <jcoolip@gmail.com>
2026-03-25 13:09:43 -0400

    added exposure settings for night mode

--- capture.py
+++ capture.py
@@ -3,20 +3,44 @@ from datetime import datetime
 
 from picamera2 import Picamera2
 
+hour = datetime.now().hour
+
+## if night time
+if hour < 6 or hour >= 18:
+    night = True
+    print("night")
+else:
+    night = False
+    print("day")
+
 
 def capture():
     now = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
     try:
         cam = Picamera2()
 
-        cam_config = cam.create_still_configuration(
-            main={"size": (1920, 1080)}, buffer_count=1
-        )
+        ## if night time
+        if night:
+            cam_config = cam.create_still_configuration(
+                main={"size": (1920, 1080)},
+                controls={
+                    "AeEnable": False,
+                    "ExposureTime": 2000000,
+                    "AnalogueGain": 8.0,
+                    "AwbEnable": True,
+                    "NoiseReductionMode": 2,
+                },
+            )
+        else:
+            cam_config = cam.create_still_configuration(
+                main={"size": (1920, 1080)}, buffer_count=1
+            )
+
         cam.configure(cam_config)
 
         cam.start()
 
-        time.sleep(2)
+        time.sleep(5)
 
         cam.capture_file(f"./data/images/{now}.jpg")
         cam.stop()
